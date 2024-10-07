6ca7428f48008179c445d1e5b6020655b28a9233
Henrik F <hnfl1438@fh-trier.de>
2024-10-07 13:26:33 +0200

    Add script for running nodes

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 
-gnome-terminal -- bash -c "source /opt/ros/humble/setup.bash ;source /home/student/Desktop/workspace/install/local_setup.bash; ros2 launch ouster_ros driver.launch.py ; exec bash"
+#gnome-terminal -- bash -c "source /opt/ros/humble/setup.bash ;source /home/student/Desktop/workspace/install/local_setup.bash; ros2 launch ouster_ros driver.launch.py ; exec bash"
 
 
-# is not working for now, Wrong bash setup?
-xterm -e "sleep 3;source /opt/ros/humble/setup.bash ;source /home/student/Desktop/workspace/install/local_setup.bash; ros2 launch zed_wrapper zed_camera.launch.py camera_model:=zed2i; exec bash"
+CURRENT_DIR=$(pwd)
 
-xterm -e "sleep 3;source /opt/ros/humble/setup.bash ;source /home/student/Desktop/workspace/install/setup.bash; ros2 run yolo_tracking object_detection_node; exec bash"
+# Open a new terminal for the first command
+xterm -e "sleep 3; cd '$CURRENT_DIR'; source install/setup.bash; source install/local_setup.bash; ros2 launch zed_wrapper zed_camera.launch.py camera_model:=zed2i; exec bash" & 
+
+# Open a new terminal for the second command
+xterm -e "sleep 3; cd '$CURRENT_DIR'; source install/setup.bash; source install/local_setup.bash; ros2 run yolo_tracking object_detection_node; exec bash" &
