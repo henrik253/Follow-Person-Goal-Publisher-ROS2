2904b384730414a2160f36c2634eacc460dd6768
Henrik F <hnfl1438@fh-trier.de>
2024-10-21 15:38:24 +0200

    Update main_launch.py

--- src/main/launch/main_launch.py
+++ src/main/launch/main_launch.py
@@ -2,12 +2,18 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(['/home/student/Desktop/workspace/src/zed-ros2-wrapper/zed_wrapper/launch/zed_camera.launch.py']),
             launch_arguments={'camera_model': 'zed2i'}.items(),
+        ),
+          IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(['/home/student/Desktop/workspace/src/ouster-ros/ouster-ros/launch/driver.launch.py']),
         ),
         Node(
             package='yolo_tracking',
